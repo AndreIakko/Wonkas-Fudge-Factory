8d7df417afc6fa620a5ed72c75c58ed1a45c1c4d
Iakkoise <63969450+AndreIakko@users.noreply.github.com>
2022-09-09 11:58:33 +0200

    Add files via upload

---
+++ iak_wonkas_fudge_factory/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version "cerulean"
+
+game "gta5"
+
+author 'Iakkoise'
+
+description "iak_wonkas_fudge_factory"
+
+this_is_a_map 'yes'
+
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+
+
+
+files {
+	"interiorproxies.meta"
+}
